c01bcab781b23b715047e6bdaa036e904137f687
redcarbb <redcarbb@gmail.com>
2025-08-04 21:48:38 +0800

    目前都OK_03

--- 02_springmvc/src/main/java/com/controller/UserController.java
+++ 02_springmvc/src/main/java/com/controller/UserController.java
@@ -20,14 +20,14 @@ import jakarta.servlet.http.HttpServletRequest;
 // @Component
 @Controller
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
 
 	@RequestMapping("/login")
 	public String login(@RequestParam("u1") String username, @RequestParam("p1") String password) {
-		// "/WEB-INF/views/login.jsp" 
-		
+		// "/WEB-INF/views/login.jsp"
+
 		System.out.println(username);
 		System.out.println(password);
 		if (userService.checkLogin(username, password)) {
@@ -35,29 +35,29 @@ public class UserController {
 		} else {
 			return "loginFail";
 		}
-		
+
 	}
-	
-	//@RequestMapping(value = "/register", method = RequestMethod.POST)
+
+	// @RequestMapping(value = "/register", method = RequestMethod.POST)
 	@PostMapping("/register")
 	public String register(@RequestParam("username") String username, @RequestParam("password") String password) {
-		
+
 		userService.addUser(username, password);
 		return "registerSuccess";
 	}
-	
+
 	@GetMapping("/toUserList")
 	public String userList(Model model) {
 		List<UserEntity> userList = userService.findAllUser();
 		model.addAttribute("userList", userList);
 		return "userList";
 	}
-	
+
 	public String userList2(HttpServletRequest req) {
-		//req.setAttribute("userList", userList);
+		// req.setAttribute("userList", userList);
 		return "";
 	}
-	
+
 	@GetMapping("/delete/{id}")
 	public String deleteUser(@PathVariable("id") Long id) {
 		System.out.println("@PathVariable id: " + id);
@@ -66,23 +66,23 @@ public class UserController {
 		// redirect:/toUserList
 		return "redirect:/toUserList";
 	}
-	
+
 	@GetMapping("/toUpdate/{id}")
 	public String toUpdatePage(@PathVariable("id") Long id, Model model) {
-		
+
 		// 透過ID 找到 USER
 		UserVo vo = userService.findUserById(id);
 		model.addAttribute("user", vo);
 		return "updateUser";
 	}
-	
+
 	@PostMapping("/update")
 	public String updateUser(UserVo user) {
 		System.out.println("UserVo: " + user);
 		// 更新資料
 		userService.upateUser(user);
-		
+
 		return "redirect:/toUserList";
-		
+
 	}
 }
--- 02_springmvc/src/main/java/com/dao/impl/UserDaoImpl.java
+++ 02_springmvc/src/main/java/com/dao/impl/UserDaoImpl.java
@@ -16,72 +16,72 @@ import com.service.DbConnectionService;
 
 @Repository
 public class UserDaoImpl implements UserDao {
-	
+
 	@Autowired
 	private DbConnectionService connectionService;
 
 	@Override
 	public UserEntity findByUsernameAndPassword(String username, String password) {
-		// 連線取得 DB 資料  
-	    
-	    Configuration configuration = new Configuration().configure();
-	    SessionFactory sessionFactory = configuration.buildSessionFactory();
-	    Session session = sessionFactory.openSession();
-	    
-	    // JPQL
-	    // SQL Injection -> preparestatement
-	    String sql = "select u from UserEntity u where u.username = ?1 and u.password = ?2";
-	    Query<UserEntity> query = session.createQuery(sql, UserEntity.class);
-	    query.setParameter(1, username);
-	    query.setParameter(2, password);
-	    List<UserEntity> userList = query.getResultList();
-		return userList != null && userList.size() > 0 ? userList.get(0) : null ;
+		// 連線取得 DB 資料
+
+		Configuration configuration = new Configuration().configure();
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+
+		// JPQL
+		// SQL Injection -> preparestatement
+		String sql = "select u from UserEntity u where u.username = ?1 and u.password = ?2";
+		Query<UserEntity> query = session.createQuery(sql, UserEntity.class);
+		query.setParameter(1, username);
+		query.setParameter(2, password);
+		List<UserEntity> userList = query.getResultList();
+		return userList != null && userList.size() > 0 ? userList.get(0) : null;
 	}
 
 	@Override
 	public void addUser(UserEntity userEnity) {
 
 		Session session = connectionService.getSession();
-		
+
 		Transaction transaction = session.beginTransaction();
-		
+
 		session.persist(userEnity);
-		
+
 		transaction.commit();
-		
+
 		session.close();
 	}
 
 	@Override
 	public void updateUser(UserEntity userEnity) {
-	    //  Try With Resource
-	    try (Session session = connectionService.getSession();) {
+		// Try With Resource
+		try (Session session = connectionService.getSession();) {
 			// 開啟交易/事務
 			Transaction transaction = session.beginTransaction();
-			
+
 			// Hibernate 6.0 以上
 			session.merge(userEnity);
 
 			// 提交
 			transaction.commit();
-	    }
-		
+		}
+
 	}
 
 	@Override
 	public void delUser(UserEntity userEnity) {
-	    //  Try With Resource
-	    try (Session session = connectionService.getSession();) {
+		// Try With Resource
+		try (Session session = connectionService.getSession();) {
 			// 開啟交易/事務
 			Transaction transaction = session.beginTransaction();
-			
+
 			// Hibernate 6.0 以上
 			session.remove(userEnity);
 
 			// 提交
 			transaction.commit();
-	    }
-		
+		}
+
 	}
 
 	@Override
@@ -89,10 +89,10 @@ public class UserDaoImpl implements UserDao {
 		try (Session session = connectionService.getSession();) {
 //		    String sql = "select u from UserEntity u";
 //		    Query<UserEntity> query = session.createQuery(sql, UserEntity.class);
-		    
-		    String sql = "select * from user";
-		    Query<UserEntity> query = session.createNativeQuery(sql, UserEntity.class);
-		    List<UserEntity> userList = query.getResultList();
+
+			String sql = "select * from user";
+			Query<UserEntity> query = session.createNativeQuery(sql, UserEntity.class);
+			List<UserEntity> userList = query.getResultList();
 			return userList;
 		}
 	}
@@ -100,10 +100,10 @@ public class UserDaoImpl implements UserDao {
 	@Override
 	public UserEntity findById(Long id) {
 		try (Session session = connectionService.getSession();) {
-		    String sql = "select * from user u where u.id = :id2 ";
-		    Query<UserEntity> query = session.createNativeQuery(sql, UserEntity.class);
-		    query.setParameter("id2", id);
-		    List<UserEntity> userList = query.getResultList();
+			String sql = "select * from user u where u.id = :id2 ";
+			Query<UserEntity> query = session.createNativeQuery(sql, UserEntity.class);
+			query.setParameter("id2", id);
+			List<UserEntity> userList = query.getResultList();
 			return userList.get(0);
 		}
 	}
